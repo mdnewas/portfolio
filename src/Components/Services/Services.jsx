e8865e2beb4a779fb7b744bbc3f856efc632dc4d
mdnewas <mdnewas11@gmail.comm>
2022-07-30 21:46:54 +0600

    complete of services and card section

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import Card from "../Card/Card";
+import heartemoji from "../../img/heartemoji.png";
+import glasses from "../../img/glasses.png";
+import humble from "../../img/humble.png";
+import Resume from './newascv.pdf'
+
 import "./Services.css";
 
 const Services = () => {
@@ -9,16 +15,45 @@ const Services = () => {
         <span>My Awesome</span>
         <span>Services</span>
         <span>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        <br />
-        Ipsum is simply dummy text of the printing and typesetting
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+          <br />
+          Ipsum is simply dummy text of the printing and typesetting
         </span>
+        <a href={Resume} download>
+
         <button className="button s-button">Download CV</button>
-        <div className="blur s-blur1" style={{background:'#ABF1FF94'}}></div>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
       </div>
       {/* right side */}
       <div className="cards">
-        I am the right side
+        {/* first card */}
+
+        <div style={{ left: "14rem" }}>
+          <Card
+            emoji={heartemoji}
+            heading={"Design"}
+            details={"Figma, Sktech, Photoshop, Adobe, Adobe Xd"}
+          />
+        </div>
+        {/* second card */}
+        <div style={{ top: "12rem", left: "-4rem" }}>
+          <Card
+            emoji={glasses}
+            heading={"Developer"}
+            details={"Html, Css, JavaScript, React"}
+          />
+        </div>
+        {/* third card */}
+        <div style={{ top: "19rem", left: "12rem" }}>
+          <Card
+            emoji={humble}
+            heading={"UI/UX"}
+            details={"UI/UX Expert"}
+          />
+        </div>
+        <div className="blur s-blur2" style={{ background: 'var(--purple)' }}></div>
       </div>
     </div>
   );
